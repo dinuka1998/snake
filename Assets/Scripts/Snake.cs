be98a3ec265ad2acc58859e17df54856929b4656
Dinuka Amarasinghe <42789915+dinuka1998@users.noreply.github.com>
2022-06-07 10:12:07 +0530

    snake controls modified

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -9,14 +9,21 @@ public class Snake : MonoBehaviour
 
     private void Update() { 
         
-        if(Input.GetKeyDown(KeyCode.W)) 
-            direction = Vector2.up;
-        else if(Input.GetKeyDown(KeyCode.S)) 
-            direction = Vector2.down;
-        else if(Input.GetKeyDown(KeyCode.A)) 
-            direction = Vector2.left;
-        else if(Input.GetKeyDown(KeyCode.D)) 
-            direction = Vector2.right;
+        if(Input.GetKeyDown(KeyCode.W)) {
+            if(!(direction == Vector2.down))
+                direction = Vector2.up;
+        } else if(Input.GetKeyDown(KeyCode.S)) {
+            if(!(direction == Vector2.up))
+                direction = Vector2.down;
+        } else if(Input.GetKeyDown(KeyCode.A)) {
+            if(!(direction == Vector2.right))
+                direction = Vector2.left;
+        } else if(Input.GetKeyDown(KeyCode.D)) {
+            if(!(direction == Vector2.left))
+                direction = Vector2.right;
+
+        }
+        
 
     }
 
