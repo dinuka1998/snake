48d80e00a3a129d68603dea959fbd80a2e90caf2
Dinuka Amarasinghe <42789915+dinuka1998@users.noreply.github.com>
2022-06-07 09:47:34 +0530

    snake movements added

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+   
+    private Vector2 direction = Vector2.right;
+
+    private void Update() { 
         
+        if(Input.GetKeyDown(KeyCode.W)) 
+            direction = Vector2.up;
+        else if(Input.GetKeyDown(KeyCode.S)) 
+            direction = Vector2.down;
+        else if(Input.GetKeyDown(KeyCode.A)) 
+            direction = Vector2.left;
+        else if(Input.GetKeyDown(KeyCode.D)) 
+            direction = Vector2.right;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void FixedUpdate() {
         
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + direction.x,
+            Mathf.Round(this.transform.position.y) + direction.y,
+            0.0f
+        );
+        
+
     }
+
 }
